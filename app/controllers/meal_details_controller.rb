c0617d42bbe5e3b969538f842f6362ef237a70b4
Janssen Radh Yumang <janssenreal23@gmail.com>
2022-07-25 17:39:14 +0800

    added youtube_addy gem and added designs

--- app/controllers/meal_details_controller.rb
+++ app/controllers/meal_details_controller.rb
@@ -10,5 +10,17 @@ class MealDetailsController < ApplicationController
   def details
     client = Meal::Api::Client.new
     @meal_details= client.meal_details(params[:id])
+    @meal_details.each do |mealDetails|
+      @youtube_link = mealDetails[1][0]["strYoutube"]
+    end
+    @youtube = YouTubeAddy.extract_video_id(@youtube_link)
+  end
+
+  def list
+    client = Meal::Api::Client.new
+    @category_details= client.meal_category(params[:category])
+    @category_details.each do |catDetails|
+      @categoryDetails = catDetails[1]
+    end
   end
 end
